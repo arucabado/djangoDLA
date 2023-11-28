debf0969f0fd0840c63a434f3d1cd0369a0becca
arucabado <55298510+arucabado@users.noreply.github.com>
2023-11-28 17:40:02 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ set -o errexit
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
