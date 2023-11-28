ed5f21cbe45d120d1960688fc3677a214073b0c1
arucabado <55298510+arucabado@users.noreply.github.com>
2023-11-28 16:47:05 -0600

    Add files via upload

--- dlaweb/settings.py
+++ dlaweb/settings.py
@@ -90,7 +90,11 @@ WSGI_APPLICATION = 'dlaweb.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config()}
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',        
+        conn_max_age=600    
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
